6275a3c0b45cb983bee17b3465c1b0a22fda378d
bhavikBonami <bhavik.jain@bonamisoftware.com>
2024-04-05 15:12:12 +0530

    changes redis broker url to docker network

--- ems/settings.py
+++ ems/settings.py
@@ -141,8 +141,7 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-# CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = 'redis://some-redis:6379'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_RESULT_EXTENDED = True
 CELERY_CACHE_BACKEND = 'django-cache'
